417846f097a3deb27cfc1c2dc018df4d9235cb35
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-11-02 12:57:26 +0100

    Solver for the "words" type

---
+++ src/js/solvers/WordsSolver.js
@@ -0,0 +1,12 @@
+export default class WordsSolver {
+    solve() {
+        window.ex.words.words.forEach((word,i) => {
+            word.use_count = 1;
+            word.use_wrong = 0;
+            word.use_level = 2;
+            word.use_last_round = (i+1);
+        });
+        window.abcd();
+        location.href = window.ex.getLinkComplete();
+    }
+}
