69571d7b44d6d79cf984912b99eac97e7ff89708
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-11-08 16:59:28 +0100

    Improved naming - it's more meaningful now

--- src/js/app.js
+++ src/js/app.js
@@ -22,7 +22,7 @@ class EmSolver {
                 <style>${appCss}</style>
                 <header class="em-solver__header">
                     <h1 class="em-solver__heading">Englishme Solver</h1>
-                    <h2 class="em-solver__subheading">Task type: <strong class="js-em-solver__task-type">${ typeTxt }</strong></h2>
+                    <h2 class="em-solver__subheading">Exercise type: <strong class="js-em-solver__exercise-type">${ typeTxt }</strong></h2>
                     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="32" height="32" viewBox="0 0 32 32" class="em-solver__close-btn">
                         <title>Close</title>
                         <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"/>
@@ -30,7 +30,7 @@ class EmSolver {
                 </header>
                 <main class="em-solver__main">
                     <div class="js-emsolver__errors-wrapper em-solver__errors-wrapper">
-                        <!-- <p class="em-solver__error">Unable to analyze the task type</p> -->
+                        <!-- <p class="em-solver__error">Unable to analyze the exercise type</p> -->
                     </div>
                     <div class="em-solver__btns-wrapper">
                         <button class="em-solver__btn js-em-solver__btn--solve"${!this.state.type ? ' disabled' : ''}>Solve everything!</button>
