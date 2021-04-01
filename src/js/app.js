5ddb6f9a50ca0263203d333981f29e630e91af9c
Vojtěch Šanda <vojtech.sanda@gmail.com>
2021-04-01 16:15:47 +0200

    Implemeted close popup feature

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,7 @@ class EmSolver {
                 <header class="em-solver__header">
                     <h1 class="em-solver__heading">Englishme Solver</h1>
                     <h2 class="em-solver__subheading">Exercise type: <strong class="js-em-solver__exercise-type">${ typeTxt }</strong></h2>
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="32" height="32" viewBox="0 0 32 32" class="em-solver__close-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="32" height="32" viewBox="0 0 32 32" class="em-solver__close-btn js-em-solver__close-btn">
                         <title>Close</title>
                         <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"/>
                     </svg>
@@ -77,6 +77,8 @@ class EmSolver {
         this.elements.solverWrapper = solverWrapper;
         this.elements.solveBtn = solverWrapper.querySelector('.js-em-solver__btn--solve');
         this.elements.fillBtn = solverWrapper.querySelector('.js-em-solver__btn--fill');
+        this.elements.closeBtn = solverWrapper.querySelector('.js-em-solver__close-btn');
+        
     }
 
     setupSolvers() {
@@ -92,6 +94,10 @@ class EmSolver {
 
         this.elements.fillBtn.addEventListener('click', () => {
             this.fill(this.state.type.code);
+        })
+
+        this.elements.closeBtn.addEventListener('click', () => {
+            this.elements.solverWrapper.parentElement.removeChild(this.elements.solverWrapper);
         })
     }
 
